a88db7699081e7dc7040004ce681c6bf4f8bd701
Chris Lee <clee5@uwhealth.org>
2018-08-03 17:11:29 -0500

    Add stylelint recommended

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,11 +2,14 @@ const order = require('./style-order.js');
 const prettier = require('./.prettierrc.js');
 
 module.exports = {
-    extends: ['stylelint-prettier/recommended'],
+    extends: ['stylelint-config-recommended', 'stylelint-prettier/recommended'],
     plugins: ['stylelint-prettier', 'stylelint-order'],
     rules: {
         'prettier/prettier': [true, prettier],
         'order/order': order.order,
         'order/properties-order': order.properties,
+        'at-rule-no-unknown': null,
+        'no-invalid-double-slash-comments': null,
+        'no-descending-specificity': null,
     },
 };
