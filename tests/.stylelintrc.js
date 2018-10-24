0f5af44ec6292083dc9e140bb58033d8f19a7f8a
cdl193 <clee5@uwhealth.org>
2018-10-24 13:58:34 -0500

    Update tests

---
+++ tests/.stylelintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+    extends: ["@uwhealth/stylelint-config"]
+}
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,34 @@
+/*
+    ---------------------------------------------
+   !!!!! Do not fix this file. !!!!
+   ---------------------------------------------
+    This is a file that is intentionally broken,
+   meant to test out eslint.
+*/
+
 var thing = 'nothing';
 
+// Console.log shouldn't be allowed
+//what shouldn't exist
 console.log(what);
 
-console.info(java);
+// if the Rhino environment is added, java should be defined
+// Shouldn't allow unused variables (no-unused-vars)
+const globalJava = java;
 
+// content should be defined as a global through t4 environemnt
 content.getStuff(thing);
+
+// No unused variables
+const clee = new thing();
+
+// shouldn't be able to reassign a const
+clee = 'Chris';
+
+// Prettier should ask for a trailing comma
+// Should also complain about the double quotes
+export const cdud = {
+    pizza: "pie",
+  // bad indentation
+  indent: "bad"
+};
