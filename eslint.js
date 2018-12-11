8093c9d548f1f48bb8134541cab82cae06766b4d
Chris Lee <clee5@uwhealth.org>
2018-12-11 11:40:37 -0600

    Import files should use node path imports

--- eslint.js
+++ eslint.js
@@ -1,1 +1,1 @@
-module.exports = require('./packages/eslint-config');
+module.exports = require('@uwhealth/eslint-config');
--- stylelint.js
+++ stylelint.js
@@ -1,1 +1,1 @@
-module.exports = require('./packages/stylelint-config');
+module.exports = require('@uwhealth/stylelint-config');
